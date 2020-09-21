a7dea9b3fbd83be1677155d8983e46a0ff6a0d9b
user <user@192.168.1.23>
2020-09-21 16:54:26 +0800

    修改好了

--- src/main/java/lendle/courses/network/clientserverbasics/DictClient.java
+++ src/main/java/lendle/courses/network/clientserverbasics/DictClient.java
@@ -21,32 +21,26 @@ public class DictClient {
 
     /**
      * @param args the command line arguments
+     * @throws java.lang.Exception
      */
     public static void main(String[] args) throws Exception {
-        // TODO code application logic here
-        Socket socket = new Socket("dict.org", 2628);
-        socket.setSoTimeout(15000);
         //hint: 從 socket 取得 OutputStream
-        OutputStream out = null;
-        ////////////////////////////////////////////
-        Writer writer = new OutputStreamWriter(out, "UTF-8");
-        //hint: 輸出 DEFINE wn gold\r\n
-        writer.write("");
-        /////////////////////////////
-        writer.flush();
-        //hint: 從socket 取得 InputStream
-        InputStream in = null;
-        ////////////////////////////////
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(in, "UTF-8"));
-        for (String line = reader.readLine(); !line.equals("."); line = reader.readLine()) {
-            //hint: 將 line 變數輸出到終端機
-            System.out.println();
-            //////////////////////
+        try ( // TODO code application logic here
+                Socket socket = new Socket("dict.org", 2628)) {
+
+            OutputStream out = socket.getOutputStream();
+            Writer writer = new OutputStreamWriter(out, "UTF-8");
+            writer.write("DEFINE wn internet\r\n");
+            writer.flush();
+            InputStream in = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(in, "UTF-8"));
+            for (String line = reader.readLine(); !line.equals("."); line = reader.readLine()) {
+                System.out.println(line);
+            }
+            writer.write("quit\r\n");
+            writer.flush();
         }
-        writer.write("quit\r\n");
-        writer.flush();
-        socket.close();
     }
 
 }
